75536e70ce637e12892c291892e14d5608bc4202
IliasMouatacim <ilias.mouatacim@gmail.com>
2026-06-16 16:12:46 +0200

    Add compositor blur masks to fix square shadow artifact behind pill panel

--- amethyst-haze-glass-theme/create_pill_panel.py
+++ amethyst-haze-glass-theme/create_pill_panel.py
@@ -13,7 +13,7 @@ svg_content = """<?xml version="1.0" encoding="UTF-8" standalone="no"?>
   <rect id="hint-left-inset" x="0" y="30" width="6" height="12" fill="none"/>
   <rect id="hint-right-inset" x="66" y="30" width="6" height="12" fill="none"/>
 
-  <!-- Elements -->
+  <!-- Visual Elements -->
   <g id="topleft">
     <rect x="0" y="0" width="30" height="30" fill="none"/>
     <path fill="#9C27B0" fill-opacity="0.35" d="M 30,6 A 24,24 0 0,0 6,30 L 30,30 Z"/>
@@ -51,6 +51,46 @@ svg_content = """<?xml version="1.0" encoding="UTF-8" standalone="no"?>
   <g id="bottomright">
     <rect x="42" y="42" width="30" height="30" fill="none"/>
     <path fill="#9C27B0" fill-opacity="0.35" d="M 66,42 A 24,24 0 0,1 42,66 L 42,42 Z"/>
+  </g>
+
+  <!-- Mask Elements for Compositor Blur/Contrast -->
+  <g id="mask-topleft">
+    <rect x="0" y="0" width="30" height="30" fill="none"/>
+    <path fill="#000000" d="M 30,6 A 24,24 0 0,0 6,30 L 30,30 Z"/>
+  </g>
+  <g id="mask-top">
+    <rect x="30" y="0" width="12" height="30" fill="none"/>
+    <rect fill="#000000" x="30" y="6" width="12" height="24"/>
+  </g>
+  <g id="mask-topright">
+    <rect x="42" y="0" width="30" height="30" fill="none"/>
+    <path fill="#000000" d="M 42,6 A 24,24 0 0,1 66,30 L 42,30 Z"/>
+  </g>
+
+  <g id="mask-left">
+    <rect x="0" y="30" width="30" height="12" fill="none"/>
+    <rect fill="#000000" x="6" y="30" width="24" height="12"/>
+  </g>
+  <g id="mask-center">
+    <rect x="30" y="30" width="12" height="12" fill="none"/>
+    <rect fill="#000000" x="30" y="30" width="12" height="12"/>
+  </g>
+  <g id="mask-right">
+    <rect x="42" y="30" width="30" height="12" fill="none"/>
+    <rect fill="#000000" x="42" y="30" width="24" height="12"/>
+  </g>
+
+  <g id="mask-bottomleft">
+    <rect x="0" y="42" width="30" height="30" fill="none"/>
+    <path fill="#000000" d="M 6,42 A 24,24 0 0,0 30,66 L 30,42 Z"/>
+  </g>
+  <g id="mask-bottom">
+    <rect x="30" y="42" width="12" height="30" fill="none"/>
+    <rect fill="#000000" x="30" y="42" width="12" height="24"/>
+  </g>
+  <g id="mask-bottomright">
+    <rect x="42" y="42" width="30" height="30" fill="none"/>
+    <path fill="#000000" d="M 66,42 A 24,24 0 0,1 42,66 L 42,42 Z"/>
   </g>
 </svg>
 """
